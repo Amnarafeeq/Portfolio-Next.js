d586e469273694e69fc6e581438ec7ad3de60634
Amnarafeeq <amnarafeeq68@gmail.com>
2024-10-08 15:36:53 +0500

    files path changed

--- src/app/homepage.tsx
+++ src/app/homepage.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Button } from "./ui/button";
+import { Button } from "../components/ui/button";
 import Typewriter from "typewriter-effect";
 import { FaCloudDownloadAlt, FaGithubSquare, FaLinkedin } from "react-icons/fa";
 import Link from "next/link";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Nav from "../components/navbar";
-import MainPage from "@/components/mainPage";
+import MainPage from "@/app/mainPage";
 
 export default function Home() {
   return (
